d7fb5ccfb64da52e2af25ef672b9d88787b9500d
Vu Tien Thanh <vutienthanh25092003@gmail.com>
2024-03-04 17:55:20 -0800

    test

---
+++ Areas/Students/Models/ManagementAssignment.cs
@@ -0,0 +1,8 @@
+﻿namespace COMP_1640.Areas.Students.Models
+{
+    public class ManagementAssignment
+    {
+        //Get title from Assigment Class
+        //Get Date start end from Assigment Class
+    }
+}
